23659c736df191756bcb44d5c0dcddccf8f45a0a
antriske <antriske@umail.iu.edu>
2017-11-14 16:57:10 -0500

    this is my message

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {AlertController, NavController} from 'ionic-angular';
+import {AboutPage} from "../about/about";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,21 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl:AlertController) {
 
   }
 
+
+  showAbout() {
+    this.navCtrl.push(AboutPage);
+  }
+
+  showAlert() {
+      let alert = this.alertCtrl.create({
+          title: 'New Friend!',
+          subTitle: 'Your friend, Hannah Montana, just accepted your friend request!',
+          buttons: ['OK']
+      });
+      alert.present();
+  }
 }
